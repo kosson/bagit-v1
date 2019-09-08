2ccaa5c1dd06c3ec099c3933bbc526633eb1bd23
Nicolaie <kosson@gmail.com>
2019-09-08 11:48:27 +0300

    Pregătiri

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const fs   = require(`fs`);
+require('dotenv').config();
+const fsPromises = require(`fs`).promises;
 const path = require(`path`);
 
+/**
+ * Clasa creează un obiect al cărui metode pot fi folosite pentru a crea Bag-uri și a le modifica
+ */
 class BagIt {
-    constructor (payload) {
-        this.payload = payload;
+    constructor (user, dir) {
+        this._version = '1.0';
+        this._tagFileCharEnc = 'UTF8';
+        this.user = user;
+        this.dir = dir;
+
+        // verifică dacă directorul există
+        // this.dirE = 
+        fs.access(this.dir, fs.F_OK, function(err) {
+            if (!err) {
+                // Do something
+            } else {
+                // It isn't accessible
+            }
+        });
     }
 
-    createBagitTXT () {
+    /**
+     * `_genrateBagitTXT()` este un utilitar intern, care trebuie apelat
+     * în cazul în care bag-ul nu există pentru a fi creat fișierul `bagit.txt`
+     * Dacă directorul deja există, nu va mai fi apelat. Se va citi fișierul.
+     */
+    static _generateBagitTXT () {
         // TODO: scrie fișierul bagit.txt
     }
 
